70fc8705d87e294846cd28c33a52492808862d2c
tienqb7716 <tientv.63.student@fit.tdc.edu.vn>
2021-12-30 14:55:14 +0700

    update
    update file public

---
+++ public/js/scriptsite.js
@@ -0,0 +1,106 @@
+
+let pageNewProduct = 1;
+let listNewProduct = document.querySelector(".list-new-product");
+let btnLoadMore = document.querySelector(".btn-loadmore-newproduct");
+let btnAddToCart = document.querySelectorAll(".btn-add-to-cart");
+const numProduct = 4;
+let productCart = document.querySelector(".product-cart");
+let totalItemCart = document.querySelector(".total-item-cart");
+if (btnLoadMore !== null) {
+    loadMoreNewProduct();
+    btnLoadMore.addEventListener("click", () => {
+        loadMoreNewProduct();
+    });
+} else {
+    addCart();
+}
+
+function loadMoreNewProduct() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+        btnLoadMore.innerText = "Đang tải...";
+        const xmlDoc = xhttp.responseText;
+        let newProducts = JSON.parse(xmlDoc);
+        renderNewProdcuts(newProducts);
+
+    }
+    xhttp.open("POST", "LoadMoreNewProductServlet", true);
+    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhttp.send(`pageNewProduct=${pageNewProduct}&numProduct=${numProduct}`);
+    pageNewProduct++;
+}
+
+function renderNewProdcuts(newProducts) {
+    if (newProducts.length < numProduct) {
+        btnLoadMore.remove();
+    } else {
+        if (newProducts.length > numProduct) {
+            newProducts.pop();
+        }
+    }
+    document.querySelectorAll('.animate__animated.animate__fadeInUp').forEach(element => {
+        element.classList.remove('animate__animated');
+    });
+    let html = '';
+    newProducts.forEach(product => {
+        html += `     <div class="col-lg-3 col-md-4 col-sm-6 mix animate__animated animate__fadeInUp  ">
+        <div class="featured__item">
+            <div class="featured__item__pic set-bg" style="border: 1px #d2cfcf61 solid;"
+                data-setbg="./public/images/product/${product.image}">
+                <ul class="featured__item__pic__hover">
+                    <li><a href="/shop-details?id=${product.id}" title="Xem chi tiết"><i
+                                class="fa fa-info-circle"></i></a></li> `
+        if (product.total != '0') {
+            html += ` <li> <a data-id="${product.id}" title="Thêm vào giỏ" style="cursor: pointer" class="btn-add-to-cart"><i class="fa fa-shopping-cart"></i></a></li >`
+        }
+        html += `  </ul>
+            </div>
+            <div class="featured__item__text">
+                <h6><a href="#">${product.name}</a></h6>
+                <h5>${product.price.toLocaleString('vi')}₫</h5>
+            </div>
+        </div>
+    </div>`
+    });
+    listNewProduct.innerHTML += html;
+    btnLoadMore.innerText = "Xem thêm";
+    loadImageProduct();
+    btnAddToCart = document.querySelectorAll(".btn-add-to-cart");
+    addCart();
+}
+function loadImageProduct() {
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+}
+function addCart() {
+    btnAddToCart.forEach(element => {
+        element.addEventListener('click', function () {
+            let id = this.dataset.id;
+            const xhttp = new XMLHttpRequest();
+            xhttp.onload = function () {
+                let obj = JSON.parse(xhttp.responseText);
+                Toastify({
+                    text: "Thêm " + obj.productName + " vào giỏ hàng",
+                    duration: 4000,
+                    newWindow: true,
+                    close: true,
+                    gravity: "top", // `top` or `bottom`
+                    position: "right",
+                    stopOnFocus: true, // Prevents dismissing of toast on hover
+                    style: {
+                        background: "linear-gradient(to right, #7fad39, #96c93d)",
+                    },
+                    onClick: function () { 
+                    }
+                }).showToast();
+                productCart.innerText = obj.amount.toLocaleString('vi') + '₫';
+                totalItemCart.innerText = obj.total;
+            }
+            xhttp.open("POST", "AddToCart", true);
+            xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            xhttp.send("idadd=" + id);
+        })
+    });
+}
